1d57b4d68076cd38edc0ffd82a293f5cbb04a030
suneel7351 <rsuneel47@gmail.com>
2022-04-05 22:29:19 +0530

    Loader change

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { CircleLoader } from "react-awesome-loaders";
+import Skeleton from "@mui/material/Skeleton";
 function Loader() {
   return (
     <div className="grid place-items-center mt-[25vh] md:mt-20">
       {" "}
-      <CircleLoader
-        meshColor={"#6366F1"}
-        lightColor={"#E0E7FF"}
-        duration={1.5}
-        desktopSize={"90px"}
-        mobileSize={"64px"}
-      />
+      <Skeleton variant="text" />
+      <Skeleton variant="circular" width={40} height={40} />
+      <Skeleton variant="rectangular" width={210} height={118} />
     </div>
   );
 }
